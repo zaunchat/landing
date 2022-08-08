64f7bb0392e6e23891e633342b927f5fd6cfae2c
z7pz <sdfygamer@gmail.com>
2022-08-08 06:47:56 +0300

    feat: add containers animation

---
+++ index.js
@@ -0,0 +1,11 @@
+ScrollReveal({ mobile: false });
+
+ScrollReveal().reveal(".nav-container", {
+  delay: 300,
+  rotate: {
+    x: 20,
+    z: 20,
+  },
+});
+ScrollReveal().reveal(".hero-container", { delay: 600, scale: 2 });
+ScrollReveal().reveal(".features-container", { delay: 900, scale: 2 });
