072155d472dcecb6372946f1b3882751d9739ae3
z7pz <sdfygamer@gmail.com>
2022-09-05 04:14:26 +0300

    feat(nextjs): initialized

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '@/styles/Landing.module.scss';
+
+export default function LandingPage() {
+  return (
+    <>
+      <div className={styles.title}>Itchat landing page</div>
+    </>
+  );
+}
