bd23a606b77bf618a2f4aab63e61be7434a91d34
z7pz <sdfygamer@gmail.com>
2022-09-06 08:23:55 +0300

    feat(landing-page): add hero

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -1,5 +1,23 @@
 import { Container } from '@components/utils';
-
+import styles from '@styles/components/landing/Hero.module.scss';
 export function Hero() {
-  return <Container>Hero</Container>;
+  return (
+    <Container backgroundColor={`transparent`}>
+      <div className={styles.hero}>
+        <div className={styles.label}>CHAT APPLICATION</div>
+        <h1 className={styles.title}>
+          MORE <span>FEATURES</span>, MORE FUN!
+        </h1>
+        <h2 className={styles.description}>
+          Playing, Communicating, Meetings are all here in our application the
+          main idea of it is to solve issues that other platforms can not
+        </h2>
+        <div className={styles.label}>
+          IN OTHER WORDS: THE MOST <span>PERFECT/BALANCED</span> CHAT PLATFORM
+          FOR ALL PEOPLE
+        </div>
+        <button>REGISTER</button>
+      </div>
+    </Container>
+  );
 }
