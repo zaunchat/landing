ab242923553e2581ebec7708f02fec11591dc2dc
z7pz <sdfygamer@gmail.com>
2022-09-06 03:09:18 +0300

    feat: navbar component compleated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,47 @@
 import { Container } from './utils';
-
+import styles from '@styles/components/Navbar.module.scss';
+import { LinkIcon } from '@components/assets';
+function NavLeftListItem({
+  title,
+  href,
+  onClick = () => {
+    return;
+  },
+}: {
+  title: string;
+  href?: string;
+  onClick?: () => void;
+}) {
+  return (
+    <div>
+      <div className={styles.title} onClick={onClick}>
+        {title}
+      </div>
+      {href && (
+        <div className={styles.icon}>
+          <LinkIcon />
+        </div>
+      )}
+    </div>
+  );
+}
 export function Navbar() {
-  return <Container>Navbar</Container>;
+  return (
+    <Container backgroundColor={`#111111`}>
+      <nav className={styles.navbar}>
+        {/* TODO?: using li list */}
+        <div className={styles.left}>
+          <div className={styles.title}>ITCHAT</div>
+          <div className={styles.divider} />
+          <div className={styles.list}>
+            <NavLeftListItem title="SUPPORT" href="/" />
+            <NavLeftListItem title="GITHUB" href="/" />
+          </div>
+        </div>
+        <div className={styles.right}>
+          <button>JOIN</button>
+        </div>
+      </nav>
+    </Container>
+  );
 }
--- src/components/utils/Container.tsx
+++ src/components/utils/Container.tsx
@@ -1,7 +1,14 @@
 import styles from '@styles/components/utils/Container.module.scss';
-export function Container({ children }: { children: any }) {
+import { CSSProperties } from 'react';
+export function Container({
+  children,
+  backgroundColor,
+}: {
+  children: any;
+  backgroundColor: CSSProperties['backgroundColor'];
+}) {
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} style={{ backgroundColor }}>
       <div className={styles.container}>{children}</div>
     </div>
   );
