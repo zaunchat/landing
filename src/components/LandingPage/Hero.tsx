ec4615ecc36b2e878006e9eeacd109b451efc9a4
z7pz <sdfygamer@gmail.com>
2022-09-06 20:37:49 +0300

    feat(landing/hero): add main hero svg

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -1,22 +1,26 @@
+import { HeroSvgIcon } from '@components/assets';
 import { Container } from '@components/utils';
 import styles from '@styles/components/landing/Hero.module.scss';
 export function Hero() {
   return (
     <Container leftBorder rightBorder backgroundColor={`transparent`}>
-      <div className={styles.hero}>
-        <div className={styles.label}>CHAT APPLICATION</div>
-        <h1 className={`${styles.title} ${styles._deth_dots}`}>
-          MORE <span>FEATURES</span>, MORE FUN!
-        </h1>
-        <h2 className={styles.description}>
-          Playing, Communicating, Meetings are all here in our application the
-          main idea of it is to solve issues that other platforms can not
-        </h2>
-        <div className={styles.label}>
-          IN OTHER WORDS: THE MOST <span>PERFECT/BALANCED</span> CHAT PLATFORM
-          FOR ALL PEOPLE
+      <div className={styles.container}>
+        <div className={styles.hero}>
+          <div className={styles.label}>CHAT APPLICATION</div>
+          <h1 className={`${styles.title} ${styles._deth_dots}`}>
+            MORE <span>FEATURES</span>, MORE FUN!
+          </h1>
+          <h2 className={styles.description}>
+            Playing, Communicating, Meetings are all here in our application the
+            main idea of it is to solve issues that other platforms can not
+          </h2>
+          <div className={styles.label}>
+            IN OTHER WORDS: THE MOST <span>PERFECT/BALANCED</span> CHAT PLATFORM
+            FOR ALL PEOPLE
+          </div>
+          <button className={styles._deth_dots}>REGISTER</button>
         </div>
-        <button className={styles._deth_dots}>REGISTER</button>
+        <HeroSvgIcon />
       </div>
     </Container>
   );
