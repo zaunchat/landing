e9e6a344682b27e4d3427b2c7a49909e97bc7d31
z7pz <sdfygamer@gmail.com>
2022-09-07 00:11:48 +0300

    fix(navbar): bug fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import { Container } from './utils';
 import styles from '@styles/components/Navbar.module.scss';
 import { LinkIcon } from '@components/assets';
+import { LegacyRef, useEffect, useRef } from 'react';
 function NavLeftListItem({
   title,
   href,
+  newWindow = false,
   onClick = () => {
     return;
   },
@@ -11,18 +13,32 @@ function NavLeftListItem({
   title: string;
   href?: string;
   onClick?: () => void;
+  newWindow?: boolean;
 }) {
+  const ref = useRef<HTMLAnchorElement>();
+  useEffect(() => {
+    if (ref.current) {
+      if (href) {
+        if (newWindow) {
+          ref.current.onclick = () => window.open(href);
+          return;
+        }
+        ref.current.href = href;
+        return;
+      }
+      ref.current.onclick = onClick;
+    }
+  }, []);
+
   return (
-    <div>
-      <div className={styles.title} onClick={onClick}>
-        {title}
-      </div>
+    <a ref={ref as LegacyRef<HTMLAnchorElement>}>
+      <span className={styles.title}>{title}</span>
       {href && (
-        <div className={styles.icon}>
+        <span className={styles.icon}>
           <LinkIcon />
-        </div>
+        </span>
       )}
-    </div>
+    </a>
   );
 }
 export function Navbar() {
@@ -34,12 +50,22 @@ export function Navbar() {
           <div className={styles.title}>ITCHAT</div>
           <div className={styles.divider} />
           <div className={styles.list}>
-            <NavLeftListItem title="SUPPORT" href="/" />
-            <NavLeftListItem title="GITHUB" href="/" />
+            <NavLeftListItem
+              title="SUPPORT"
+              href="https://github.com/itchatapp"
+              newWindow
+            />
+            <NavLeftListItem
+              title="GITHUB"
+              href="https://github.com/itchatapp"
+              newWindow
+            />
           </div>
         </div>
         <div className={styles.right}>
-          <button>JOIN</button>
+          <button onClick={() => (window.location.href = `#register`)}>
+            JOIN
+          </button>
         </div>
       </nav>
     </Container>
