a4f5ef7d859ef5c28fd58b84ea3024a959a57efe
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-09-15 17:59:18 +0000

    fix(footer): add missing links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,27 +15,27 @@ export function Footer() {
           <MicosoftStoreIcon />
         </div>
         <div className={styles.list}>
-          <div className={styles.title}>Repositories</div>
+          <div className={styles.title}>Developers</div>
           <ul className={styles.items}>
-            <li><a href="#">API</a></li>
-            <li><a href="#">Client</a></li>
-            <li><a href="#">Landing</a></li>
+            <li><a href="https://docs.itchat.world/">API Docs</a></li>
+            <li><a href="https://github.com/itchatapp/itchat.js">JavaScript Client</a></li>
+            <li><a href="https://github.com/itchatapp/itchat.py">Python Client</a></li>
           </ul>
         </div>
         <div className={styles.list}>
           <div className={styles.title}>Products</div>
           <ul className={styles.items}>
-            <li><a href="#">Application</a></li>
-            <li><a href="#">Login</a></li>
-            <li><a href="#">Landing</a></li>
+            <li><a href="https://app.itchat.world">Application</a></li>
+            <li><a href="https://app.itchat.world/login">Login</a></li>
           </ul>
         </div>
         <div className={styles.list}>
           <div className={styles.title}>Follow Us</div>
           <ul className={styles.items}>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">Telegram</a></li>
-            <li><a href="#">Discord</a></li>
+            <li><a href="https://twitter.com/itchatapp">Twitter</a></li>
+            <li><a href="https://reddit.com/r/itchatapp">Reddit</a></li>
+            <li><a href="https://discord.gg/nNHH5apCwr">Discord</a></li>
+            <li><a href="https://matrix.to/#/#itchatapp:matrix.org">Matrix</a></li>
             <li><a href="https://github.com/itchatapp">Github</a></li>
           </ul>
         </div>
