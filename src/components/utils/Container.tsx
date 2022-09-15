4bf46564320a168b6228ad09f3c7d48dc0a8c020
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-09-15 17:44:48 +0000

    refactor(container): Simplify things

--- src/components/utils/Container.tsx
+++ src/components/utils/Container.tsx
@@ -1,23 +1,22 @@
 import styles from '@styles/components/utils/Container.module.scss';
 import { CSSProperties } from 'react';
-export function Container({
-  children,
-  backgroundColor,
-  leftBorder = false,
-  rightBorder = false,
-}: {
-  children: any;
+
+interface Props {
+  children: unknown;
   backgroundColor: CSSProperties['backgroundColor'];
   leftBorder?: boolean;
   rightBorder?: boolean;
-}) {
+}
+
+export function Container({
+  children,
+  backgroundColor,
+  leftBorder,
+  rightBorder
+}: Props) {
   return (
     <div className={styles.wrapper} style={{ backgroundColor }}>
-      <div className={styles.container}>
-        {leftBorder && <div className={styles.left_border} />}
-        {children}
-        {rightBorder && <div className={styles.right_border} />}
-      </div>
+      <div className={[styles.container, leftBorder && styles.left_border, rightBorder && styles.right_border].filter(Boolean).join(' ')}>{children}</div>
     </div>
   );
 }
